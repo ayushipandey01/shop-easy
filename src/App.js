aa7fb64a4704f09e6fd12b181e553a611edb2f9a
Ayushi Pandey <ayushipandey75@gmail.com>
2023-03-29 20:31:13 +0530

    subheader section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Products } from './components/Products/Products'
+import { SubHeader } from './components/SubHeader/SubHeader';
 
 function App() {
   return (
     <div>
       <Header />
+      <SubHeader />
       <Products />
     </div>    
   );
