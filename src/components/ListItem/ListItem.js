9851dc3605542e1bc90d811a891b675ab7319c2c
Ayushi Pandey <ayushipandey75@gmail.com>
2023-03-30 22:16:37 +0530

    form handling and two way binding

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import addToCart from '../../assets/cart.png';
 
 export const ListItem = (props)=>{
 
     const { data } = props; //object destructuring
+
+    // const [showMessage , setShowMessage] = useState("Not added to cart yet");
+
+    // const clickHandler = (()=>{
+    //     setShowMessage("Item added to Cart !");
+    // })
+
+    const [counter , setCounter] = useState(0);
+
+    const descreaseCounterByOne = ()=>{
+        if(counter <= 0){
+            return;
+        }
+        setCounter(counter => counter - 1);
+    }
+
+    const increaseCounterByOne = ()=>{
+        setCounter(counter => counter + 1);
+    }
 
     return(
         <div className={'item-card'}>
@@ -16,10 +36,16 @@ export const ListItem = (props)=>{
                     <h3>{data.title}</h3>
                 </div>
             </div>
-            <button className={'cart-add'}>
+            {/* <small className='cart-message'>{showMessage}</small> */}
+            {/* <button onClick={clickHandler} className={'cart-add'}>
                 <span>Add to cart</span>
                 <img className = "cart-icon" src ={addToCart} alt="cart-icon" />
-            </button>
+            </button> */}
+            <div className={"cart-addon"}>
+                <button onClick={descreaseCounterByOne}><span>-</span></button>
+                <span className={"counter"}>{counter}</span>
+                <button onClick={increaseCounterByOne}><span>+</span></button>
+            </div>
         </div>
     )
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,29 +1,85 @@
+import { useState } from "react"
 import { ListItem } from "../ListItem/ListItem"
  
 export const Products = ()=>{
 
-    const items = [
+    const [title , setTitle] = useState("");
+    const [price , setPrice] = useState(0);
+    const [discountedPrice , setdiscountedPrice] = useState(0);
+    const [thumbnail , setThumbnail] = useState("");
+
+    const [items , setItems] = useState(
         {
-            id:1,
-            price: 540,
+            id:0,
+            price: 0,
             discountedPrice: 400,
             title: "Title of item-1",
             thumbnail : "/assets/placeholder.jpg"
-        },
-        {
-            id:2,
-            price: 550,
-            discountedPrice: 460,
-            title: "Title of item-2",
-            thumbnail : "/assets/placeholder.jpg"
+    })        
+    
+    const handleInput = (event)=>{
+        setTitle(event.target.value);
+    }
+    const handlePrice = (event)=>{
+        setPrice(event.target.value);
+    }
+    const handleDiscountedPrice = (event)=>{
+        setdiscountedPrice(event.target.value);
+    }
+    const handleThumbnail = (event)=>{
+        setThumbnail(event.target.value);
+    }
+
+    const submitForm = event=>{
+        event.preventDefault();
+
+        if(discountedPrice > price){
+            alert("Discounted price can't be greater than price")
         }
-    ]
+        setItems( {
+            title: title,
+            price,
+            discountedPrice,
+            thumbnail
+        })
+    }
+
     return (
-        <div className={'product-list'}>
-            <div className={'product-list--wrapper'}>
-                <ListItem data = {items[0]}></ListItem>
-                <ListItem data = {items[1]}></ListItem>
-            </div>            
+
+        <div className="product-wrapper">
+            <div className="form">
+                <form onSubmit={submitForm}>
+                    <h2>Title of the Form</h2>
+                    <div className="input-field">
+                        <label htmlFor="title">Title</label>
+                        <input type="text" placeholder="type title" value = {title} onChange={handleInput} required/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="price">Price</label>
+                        <input type="number" placeholder="enter price" value = {price} onChange={handlePrice} required/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="discountedPrice">Discount Price</label>
+                        <input type="number" placeholder="enter Discount price" value = {discountedPrice} onChange={handleDiscountedPrice} required/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="Thumbnail">Thumbnail</label>
+                        <input type="" placeholder="enter Thumbnail" value = {thumbnail} onChange={handleThumbnail} required/>
+                    </div>
+                    <div className="submit-wrap">
+                        <button>Update</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <div className>
+                    <ListItem data = {items}></ListItem>
+                </div>  
+                {/* <div className={'product-list--wrapper'}>
+                    <ListItem data = {items[0]}></ListItem>
+                </div>             */}
+            </div>
         </div>
+        
     )
 }
